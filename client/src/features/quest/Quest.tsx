a3299053abafc385f58e9cddb4ee30cce284305e
jonathanng1122 <jonathanng1122@gmail.com>
2021-08-29 16:13:41 -0700

    refactored tasksJSX

--- client/src/features/quest/Quest.tsx
+++ client/src/features/quest/Quest.tsx
@@ -6,31 +6,36 @@ import {
 
 } from './questSlice';
 import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { AppDispatch } from "../../app/store";
 
 export function Quest() {
     const title = useAppSelector(selectTitle);
     const tasks = useAppSelector(selectTasks);
     const dispatch = useAppDispatch();
 
-    const tasksJSX = [];
-    for (let i = 0; i < tasks.length; i++) {
-        tasksJSX.push(
-            <div key={i}>
-                <label htmlFor="completed">{tasks[i].name}</label>
-                <input type="checkbox" name="completed" checked={tasks[i].completed} onChange={() => {
-                    let task = {
-                        name: tasks[i].name,
-                        completed: !tasks[i].completed
-                    }
-                    dispatch(changeTask({ num: i, task }))
-                }} />
-            </div>
-        )
-    }
     return (
         <div>
             <h1>{title}</h1>
-            {tasksJSX}
+            {
+                (() => {
+                    let tasksJSX = [];
+                    for (let i = 0; i < tasks.length; i++) {
+                        tasksJSX.push(
+                            <div key={i}>
+                                <label htmlFor="completed">{tasks[i].name}</label>
+                                <input type="checkbox" name="completed" checked={tasks[i].completed} onChange={() => {
+                                    let task = {
+                                        name: tasks[i].name,
+                                        completed: !tasks[i].completed
+                                    }
+                                    dispatch(changeTask({ num: i, task }))
+                                }} />
+                            </div>
+                        )
+                    }
+                    return tasksJSX
+                })()
+            }
         </div>
     )
 }
