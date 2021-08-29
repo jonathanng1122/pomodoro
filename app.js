243c6e8d6f0ba84cc43f5abbe6399c3f4e46fa39
jonathanng1122 <jonathanng1122@gmail.com>
2021-08-28 21:52:00 -0700

    connected mongodb

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 5000 || process.env.port;
-
+const port = 5000 || process.env.PORT;
+const {MONGODB_URI} = require('./config.js')
 
 app.use('/api/users', require("./routes/users.js"))
+
+mongoose.connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true, //uses server selection whatever that means... does algorthims
+})
+mongoose.connection.on('connected', () => {
+    console.log('Connected to MongoDB');
+});
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
-router.get('/', (req, res) => {
-    res.send('Hello World!');
+router.get('/', async (req, res) => {
+    const users = await User.find({});
+    return res.json({users})
 })
+
 
 module.exports = router;
