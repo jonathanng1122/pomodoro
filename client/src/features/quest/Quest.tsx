ed9ab5049151876121a7f9822b5cf113bfc0a27a
jonathanng1122 <jonathanng1122@gmail.com>
2021-08-29 10:52:57 -0700

    added state change for quests

--- client/src/features/quest/Quest.tsx
+++ client/src/features/quest/Quest.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { changeTitle, selectTitle } from './questSlice';
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
 
 export function Quest() {
+    const title = useAppSelector(selectTitle);
+    const dispatch = useAppDispatch();
     return (
         <div>
-            Quests will go here
+            <h1>{title}</h1>
+            <input type="text" value={title} onChange={e => dispatch(changeTitle(e.currentTarget.value))} />
         </div>
     )
 }
