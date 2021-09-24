d939c9e28652c4a76db9321c8853262b66655df9
jonathanng1122 <jonathanng1122@gmail.com>
2021-09-23 21:21:10 -0700

    added basic tests for interval api

--- client/src/features/timer/TimerAPI.test.ts
+++ client/src/features/timer/TimerAPI.test.ts
@@ -9,22 +9,27 @@ it('gets all intervals', async () => {
     expect.assertions(2);
     const res = await getAllIntervals();
     expect(res).toBeTruthy();
-    expect(res.data).toBeTruthy();
+    return expect(res.data).toBeTruthy();
 })
 
 
 it('creates a new interval and getting it', async () => {
-    expect.assertions(2);
+    expect.assertions(3);
     const msec = new Date().getTime();
     const interval: Interval = {
         start: msec,
         userId: "612b111b881930941e73e029"
     }
     const res1 = await createNewInterval(interval);
-    expect(res1.data.interval._id).toBeTruthy();
-    if (res1.data.interval._id) {
-        const res = await getInterval(res1.data.interval._id);
-        console.log(res.data)
+    const intervalId = res1.data.interval._id
+    expect(intervalId).toBeTruthy();
+    if (intervalId) {
+        const res = await getInterval(intervalId);
         expect(new Date(res1.data.interval.start).getTime()).toEqual(msec);
+        interval.start = msec + 12345
+        const updateRes = await updateInterval(intervalId, interval)
+        return expect(new Date(updateRes.data.interval.start).getTime()).toEqual(interval.start)
     }
 })
+
+//TODO: create test for update interval
